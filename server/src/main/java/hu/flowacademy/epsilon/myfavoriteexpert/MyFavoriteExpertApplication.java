17ad639fcfe89710ce0e3b1a9693103ea76d337a
Boros Robert <robbie13.boros@gmail.com>
2019-06-05 10:42:37 +0200

    init project sekelton

---
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/MyFavoriteExpertApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.epsilon.myfavoriteexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavoriteExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavoriteExpertApplication.class, args);
+	}
+
+}
