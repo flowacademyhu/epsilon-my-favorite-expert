2c4be4629cb8329315baa9e75bc60821ba8c340f
hajnalandor <hajnal.andor1993@gmail.com>
2019-06-07 09:50:55 +0200

    configaration

--- server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/config/SecurityConfig.java
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/config/SecurityConfig.java
@@ -1,11 +1,27 @@
 package hu.flowacademy.epsilon.myfavoriteexpert.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+import hu.flowacademy.epsilon.myfavoriteexpert.security.OAuth2AuthenticationSuccessHandler;
+import hu.flowacademy.epsilon.myfavoriteexpert.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import hu.flowacademy.epsilon.myfavoriteexpert.security.oauth2.OAuth2AuthenticationFailureHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/*@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
@@ -19,5 +35,65 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().permitAll()
         .and().formLogin().permitAll()
         .and().csrf().disable();
+  }
+
+
+}*/
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+  @Autowired
+  private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+  @Autowired
+  private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+  @Autowired
+  private HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+
+
+  /*
+    By default, Spring OAuth2 uses HttpSessionOAuth2AuthorizationRequestRepository to save
+    the authorization request. But, since our service is stateless, we can't save it in
+    the session. We'll save the request in a Base64 encoded cookie instead.
+  */
+  @Bean
+  public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
+    return new HttpCookieOAuth2AuthorizationRequestRepository();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+  @Bean(BeanIds.AUTHENTICATION_MANAGER)
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .anyRequest().permitAll()
+            .and().formLogin().permitAll()
+            .and().csrf().disable();
+
   }
 }
--- server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/config/WebMvcConfig.java
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/config/WebMvcConfig.java
@@ -1,12 +1,15 @@
 package hu.flowacademy.epsilon.myfavoriteexpert.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+  //TODO CORS
+  private final long MAX_AGE_SECS = 3600;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -15,5 +18,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     registry.addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS);
   }
 }
