d69fd0e5da65a196c2739669868b56f1fe58ec91
farkasanita <farkasanita@outlook.com>
2019-06-07 14:55:44 +0200

    hu/e

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+   }
+
+   switchLanguage(language: string) {
+    this.translate.use(language);
+}
 
   ngOnInit() {
   }
