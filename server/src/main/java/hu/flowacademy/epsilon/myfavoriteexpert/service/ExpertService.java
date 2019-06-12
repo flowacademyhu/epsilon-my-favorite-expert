dcc1e506a225fc1796c901433ecbae24e018f4af
wunderszindi <sandrine.varga@gmail.com>
2019-06-12 16:29:28 +0200

    add ExpertService

---
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/service/ExpertService.java
@@ -0,0 +1,54 @@
+package hu.flowacademy.epsilon.myfavoriteexpert.service;
+
+
+import hu.flowacademy.epsilon.myfavoriteexpert.model.Expert;
+import hu.flowacademy.epsilon.myfavoriteexpert.repository.ExpertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ExpertService {
+
+    @Autowired
+    private ExpertRepository expertRepository;
+
+    public Expert save(Expert expert) {
+        expert.setId(UUID.randomUUID());
+        return expertRepository.save(expert);
+
+    }
+
+    public Optional<Expert> findById(UUID id) {
+        return expertRepository.findById(id);
+    }
+
+    public Iterable<Expert> list() {
+        return expertRepository.findAll();
+    }
+
+    public void delete(UUID id) {
+        Optional<Expert> expert = expertRepository.findById(id);
+        if (expert.isPresent()) {
+            expert.get().setIs_deleted(true);
+            expertRepository.save(expert.get());
+        } else {
+            throw new RuntimeException("expert does not exist!");
+        }
+    }
+
+    public void addProfession(UUID id, String profession) {
+        Optional<Expert> expert = expertRepository.findById(id);
+        if(expert.isPresent()) {
+            expert.get().setProfession(List.of(profession));
+            expertRepository.save(expert.get());
+        }
+    }
+
+
+
+}
