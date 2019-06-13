bff3612ebd2a7b89c646c0e407019bf20b0a6a1b
farkasanita <farkasanita@outlook.com>
2019-06-13 11:23:27 +0200

    profile

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoggedinComponent } from './components/loggedin/loggedin.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
-  {path:'oauth2/redirect', component:LoggedinComponent}];
+  {path:'oauth2/redirect', component:LoggedinComponent},
+{path:'profile', component:ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
