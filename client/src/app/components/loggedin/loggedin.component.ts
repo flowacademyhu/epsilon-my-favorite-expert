b861ddfce4f4b2f9f95d6ef34826a7b5bdfb0780
farkasanita <farkasanita@outlook.com>
2019-06-13 15:07:51 +0200

    token

--- client/src/app/components/loggedin/loggedin.component.ts
+++ client/src/app/components/loggedin/loggedin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from 'src/app/data.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-loggedin',
@@ -7,9 +9,19 @@ import { DataService } from 'src/app/data.service';
   styleUrls: ['./loggedin.component.css']
 })
 export class LoggedinComponent implements OnInit {
-  
-  constructor() { }
+
+token: any;
+
+  constructor(private route: ActivatedRoute, private userservice: UserService) {
+    
+   }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+      console.log(this.token);
+    });
+    this.userservice.saveToken(this.token);
   }
+
 }
