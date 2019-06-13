c5aa8ba3981725dd41b173795253fcb64442c9c5
hajnalandor <hajnal.andor1993@gmail.com>
2019-06-13 11:26:29 +0200

    save address endpoint and findbyid endpoint added

--- server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/controller/UserElasticController.java
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/controller/UserElasticController.java
@@ -1,15 +1,14 @@
 package hu.flowacademy.epsilon.myfavoriteexpert.controller;
 
+import hu.flowacademy.epsilon.myfavoriteexpert.model.Address;
 import hu.flowacademy.epsilon.myfavoriteexpert.model.UserElastic;
 import hu.flowacademy.epsilon.myfavoriteexpert.service.UserElasticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/auth")
+@RequestMapping(path = "/auth/user")
 public class UserElasticController {
 
     @Autowired
@@ -20,4 +19,13 @@ public class UserElasticController {
         return ResponseEntity.ok(userElasticService.find());
     }
 
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<UserElastic> getById(@PathVariable String id) {
+        return ResponseEntity.ok(userElasticService.findByid(id));
+    }
+    @PostMapping("/save-address/{id}")
+    public ResponseEntity<UserElastic> saveAddress(@PathVariable String id, @RequestBody Address address) {
+        return ResponseEntity.ok(userElasticService.saveAddress(id,address));
+    }
 }
