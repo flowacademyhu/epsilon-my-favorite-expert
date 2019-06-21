225238fc7fb16685492ef48902a52ccca647dc15
Poczok <tamaspece@gmail.com>
2019-06-21 13:55:19 +0200

    displaying favorites of a user WIP

--- client/src/app/components/listing/user/user.component.ts
+++ client/src/app/components/listing/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ExpertService } from 'src/app/shared/services/expert.service';
+import { Expert } from 'src/app/models/expert.model';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +10,29 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UserComponent implements OnInit {
   @Input()
   user: any;
-  constructor() { }
+
+  experts: Expert[] = [];
+
+  favoriteExpert: Expert[] = [];
+
+  showFavorites = false;
+
+  constructor(private expertService: ExpertService) { }
 
   ngOnInit() {
   }
-
+  getAllExperts() {
+    this.loadData();
+    if (this.showFavorites === false){
+        this.showFavorites = true;
+    } else {
+      this.showFavorites = false;
+    }
+  }
+  loadData() {
+      this.expertService.getFavoriteExperts().subscribe(
+        (data: Expert[]) => {
+          this.favoriteExpert = data;
+        });
+  }
 }
