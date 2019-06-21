74c1b344f5806f4ff66699590e2167d406b522d0
wunderszindi <sandrine.varga@gmail.com>
2019-06-21 11:20:18 +0200

    validate deleted

--- server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/service/UserService.java
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/service/UserService.java
@@ -82,7 +82,11 @@ public class UserService {
         User user = userRepository.findById(getCurrentUserId()).orElseThrow(RuntimeException::new);
         if (user == null) {
             throw new RuntimeException("User not found, id is invalid");
-        } else {
+        }
+        else if(user.getDeletedAt() != null ) {
+            throw new RuntimeException("User not found, id is invalid");
+        }
+        else {
             if(language.equalsIgnoreCase(Language.HU.toString())){
                 user.setLanguage("HU");
             }
