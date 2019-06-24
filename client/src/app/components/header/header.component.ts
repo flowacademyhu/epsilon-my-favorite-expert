4affb002f33e303ffa148a3d470dfe3903c8a051
hajnalandor <hajnal.andor1993@gmail.com>
2019-06-24 22:12:58 +0200

    logout

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { AppStateService } from 'src/app/shared/services/app-state.service';
 import { GeolocationService } from 'src/app/shared/services/geolocation.service';
 import { User, UsersResourceService } from 'src/app/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -16,7 +17,8 @@ export class HeaderComponent implements OnInit {
   user: User;
 
   constructor(private translate: TranslateService, private appState: AppStateService, private geolocation: GeolocationService,
-    private userResource: UsersResourceService) {
+    private userResource: UsersResourceService,
+    private router: Router) {
     this.state = this.appState;
    }
    
@@ -28,10 +30,13 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
-    
+
   }
   logout() {
-   this.appState = null;
+   this.appState = undefined;
    localStorage.removeItem('token');
+  localStorage.clear();
+  console.log(localStorage.getItem('token'));
+    this.router.navigate((['']));
   }
 }
