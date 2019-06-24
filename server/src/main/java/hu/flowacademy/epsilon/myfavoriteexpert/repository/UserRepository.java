9c4e58ec5c4b691e5204aca9d065087885f7e806
wunderszindi <sandrine.varga@gmail.com>
2019-06-24 17:06:27 +0200

    add changes

--- server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/repository/UserRepository.java
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/repository/UserRepository.java
@@ -19,10 +19,12 @@ public interface UserRepository extends ElasticsearchCrudRepository<User, UUID> 
 
 
 
-    @Query("{\"function_score\": {\"query\": {\"multi_match\": {\"query\": \"?0\",\"type\": \"best_fields\", \"fields\": [\"name\"],\"fuzziness\" : \"auto\"} }}}")
-    Page<User> findBestMatchesUser(String params, Pageable pageable);
+//    @Query("{\"function_score\": {\"query\": {\"multi_match\": {\"query\": \"?0\",\"type\": \"best_fields\", \"fields\": [\"name\"],\"fuzziness\" : \"auto\"} }}}")
+//    Page<User> findBestMatchesUser(String params, Pageable pageable);
 
     @Query("{\"function_score\": {\"query\": {\"multi_match\": {\"query\": \"?0\",\"type\": \"best_fields\", \"fields\": [\"name\",\"address.city^5\",\"address.country^10\",\"address.street\",\"address.number\",\"phone\",\"profession^17\"],\"fuzziness\" : \"auto\"} }}}")
     Page<User> findExpertsByUser(String params,Pageable pageable);
 
+    @Query("\"dis_max\", \"boost\", \"queries\" : [{\"bool\": {\"should\": [{\"wildcard\": {\"name\": {\"value\": \"*nd*\"}}},{\"wildcard\": {\"address.country\": {\"value\": \"*mar*\"}}},{\"wildcard\": {\"address.city\": {\"value\": \"*mar*\"}}},{\"wildcard\": {\"address.street\": {\"value\": \"*mar*\"}}},{\"wildcard\": {\"email\": {\"value\": \"*mar*\"}}}]}},: 1.2,: \"tie_breaker\" : 0.7,{\"function_score\": {\"query\": {\"multi_match\": {\"query\": \"?0\",\"type\": \"best_fields\",\"fields\": [\"name\", \"address,city\",\"address.country\",\"email\"],\"fuzziness\" : \"auto\"}}}}")
+    Page<User> findBestMatchesUser(String params,Pageable pageable);
 }
