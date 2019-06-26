f4534feae3658cc9ad0e6d518fcf5727c5432bd9
wunderszindi <sandrine.varga@gmail.com>
2019-06-26 10:20:47 +0200

    fix swagger + typo

--- server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/service/ExpertService.java
+++ server/src/main/java/hu/flowacademy/epsilon/myfavoriteexpert/service/ExpertService.java
@@ -15,14 +15,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-=======
 import java.util.*;
 import java.util.concurrent.TimeUnit;
->>>>>>> feature/sandbox_V2
 import java.util.stream.Collectors;
 
 @Slf4j
