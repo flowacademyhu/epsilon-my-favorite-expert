e43d0f35ee5d4ec84a3da20a4ba3f6ba5c0339c6
hajnalandor <hajnal.andor1993@gmail.com>
2019-06-27 20:18:31 +0200

    service tests

--- server/src/test/java/hu/flowacademy/epsilon/myfavoriteexpert/service/UserServiceTest.java
+++ server/src/test/java/hu/flowacademy/epsilon/myfavoriteexpert/service/UserServiceTest.java
@@ -6,6 +6,8 @@ import hu.flowacademy.epsilon.myfavoriteexpert.model.User;
 import hu.flowacademy.epsilon.myfavoriteexpert.repository.ExpertRepository;
 import hu.flowacademy.epsilon.myfavoriteexpert.repository.UserRepository;
 import hu.flowacademy.epsilon.myfavoriteexpert.security.UserPrincipal;
+import hu.flowacademy.epsilon.myfavoriteexpert.service.geocoding.GeoCodingService;
+import hu.flowacademy.epsilon.myfavoriteexpert.service.geocoding.Location;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -51,6 +54,8 @@ public class UserServiceTest {
     User user;
     UUID id = UUID.randomUUID();
 
+    @Mock
+    private GeoCodingService geoCodingService;
 
     @Before
     public void setup() {
@@ -70,7 +75,8 @@ public class UserServiceTest {
 
         when(userRepository.save(any(User.class))).thenReturn(user);
         when(userRepository.findById(id)).thenReturn(java.util.Optional.ofNullable(user));
-        when(expertRepository.findById(expertId)).thenReturn(java.util.Optional.of(expert));
+       // when(expertRepository.findById(expertId)).thenReturn(java.util.Optional.of(expert));
+
     }
 
 
@@ -100,48 +106,68 @@ public class UserServiceTest {
         assertEquals(userTest.getAddress().toString(), user.getAddress().toString());
     }
 
+    @Test(expected = RuntimeException.class)
+    public void saveAddres_ShouldThrowRuntimeExeption() {
+        Address address = new Address();
+        address.setCountry("M");
+        address.setCity("Ó");
+        address.setStreet("Be utca");
+        address.setNumber("4");
+
+        userService.saveAddress(address);
+    }
+
+    @Test()
+    public void saveAddress_shouldReturnUser() {
+        Address address = new Address();
+        address.setCountry("Magyarország");
+        address.setCity("Szeged");
+        address.setStreet("Vedres utca");
+        address.setNumber("3");
+        Location loc = new Location();
+        loc.setLat(21.);
+        loc.setLon(33.);
+        user.setAddress(address);
+        when(geoCodingService.getGeoCoding(address)).thenReturn(loc);
+        assertEquals(userService.saveAddress(address),user);
+    }
+
     @Test
     public void deleteExpert_ShouldReturnUser() {
         UUID id = UUID.randomUUID();
         UUID id2 = UUID.randomUUID();
+        User userTest = user;
+        userTest.addExpert(id);
         user.addExpert(id);
         user.addExpert(id2);
-
-        user.deleteExpert(id);
-        assertEquals(user.getExperts().isEmpty(), false);
+        assertEquals(userTest, userService.deleteExpert(user,id2));
     }
-    @Test
-    public void addFollower_ShouldReturnFollower() {
-        UUID id1 = UUID.randomUUID();
-        UUID id2 = UUID.randomUUID();
-        user.addFollower(id1);
-        user.addFollower(id2);
 
-        assertEquals(user.getFollowers().contains(id1), true);
-        assertEquals(user.getFollowers().contains(id2), true);
-    }
     @Test
     public void deleteFollower_ShouldReturnUser() {
-        UUID id1 = UUID.randomUUID();
-        UUID id2 = UUID.randomUUID();
-        user.addFollower(id1);
-        user.addFollower(id2);
-        user.deleteFollower(id1);
-
-        assertFalse(user.getFollowers().contains(id1));
-        assertTrue(user.getFollowers().contains(id2));
+        User userTest= user;
+        user.addFollower(user.getId());
+        user.addFollowedBy(user.getId());
+        assertEquals(userService.deleteFollower(user,user.getId()),userTest);
     }
+
 
     @Test
     public void saveLanguageHU_ShouldReturnUser() {
-        user = userService.saveLanguage("HU");
-        assertEquals(user.getLanguage(), "HU");
+        var testUser = user;
+        testUser.setLanguage("HU");
+        assertEquals(userService.saveLanguage("HU"), testUser);
     }
+
     @Test
     public void saveLanguageEN_ShouldReturnUser() {
-        user = userService.saveLanguage("EN");
-        assertEquals(user.getLanguage(), "EN");
+        var testUser = user;
+        testUser.setLanguage("EN");
+        assertEquals(userService.saveLanguage("EN"), testUser);
     }
+
+
+
 
 
 
